e5f2aeebcf863f533817ec9f03191f322aa78c5e
mt854 <mt854@ELCLASS23.its.york.ac.uk>
2015-01-27 16:05:43 +0000

    added new message
    Signed-off-by: mt854 <mt854@ELCLASS23.its.york.ac.uk>

--- test/src/HelloWorld.java
+++ test/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 		public static void main(String[] args) {
 			System.out.println("Hello World!");
 			System.out.println("Tom doesn't know how to use github");
+			System.out.println("You figured it out!!!");
 		}
 		
 }
