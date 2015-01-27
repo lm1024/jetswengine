599a20686c4556d91d9b4545582ab269bc00aa0c
solomonatron <tomdavidson@live.co.uk>
2015-01-27 15:50:20 +0000

    Still playing with git
    Signed-off-by: solomonatron <tomdavidson@live.co.uk>

--- test/src/HelloWorld.java
+++ test/src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 
 		public static void main(String[] args) {
 			System.out.println("Hello World!");
+			System.out.println("Tom doesn't know how to use github");
 		}
 		
 }
