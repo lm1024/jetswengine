9ef315f32fa366ef6d477cbc918da3d4e00a0766
solomonatron <tomdavidson@live.co.uk>
2015-01-28 14:27:17 +0000

    Updated all the practice project files to change block comments to doc comments so nice tooltips are made
    Signed-off-by: solomonatron <tomdavidson@live.co.uk>

--- Practice Projects/jod512/src/TestClass.java
+++ Practice Projects/jod512/src/TestClass.java
@@ -3,28 +3,29 @@
  */
 class TestClass {
 	/*
-	 * This block comment should contain any class-wide or interface-wide information
-	 * that wasn’t appropriate for the class/interface documentation comment.
+	 * This block comment should contain any class-wide or interface-wide
+	 * information that wasn’t appropriate for the class/interface documentation
+	 * comment.
 	 * 
 	 * If you need to give information about a class, interface, variable, or
 	 * method that isn’t appropriate for documentation, use an implementation
 	 * block comment or single-line comment immediately after the declaration
 	 * (like this).
 	 */
-	
+
 	/* All numbers that are not -1, 0 or 1 declared here as statics */
-	private static final int MAX_NUMBER_OF_GREEN_BOTTLES = 2;
-	
+	private static final int MAX_NUMBER_OF_GREEN_BOTTLES = 10;
+
 	static int numberOfGreenBottles; // One declaration per line
-	
+
 	int numberOfFingersOnOneHand; // Small comment if required
-	
-	/* Single line block comment to describe method. */
+
+	/** Single line block comment to describe method. */
 	public static void main(String[] args) {
 		int n = 0; // Initialise where declared if possible
- 
-		n++; 
-		
+
+		n++;
+
 		if (n == 1) {
 			sing();
 		} else if (n == 2) {
@@ -32,18 +33,16 @@ class TestClass {
 		} else {
 			System.out.println("N is not one or two.");
 		}
-		System.out.println("Hello World");
 	}
 
-	/*
-	* Longer block comment to describe method.
-	* method sing() sings a song!
-	*/
+	/**
+	 * Longer block comment to describe method. method sing() sings a song!
+	 */
 	protected static void sing() {
-		/* 
-		 * Loops through and sings all the verses of the song 
-		 * Code over the 80 char limit can be formatted using CTRL + SHIFT + F
-		 * and it always looks horrible.
+		/*
+		 * Loops through and sings all the verses of the song Code over the 80
+		 * char limit can be formatted using CTRL + SHIFT + F and it always
+		 * looks horrible.
 		 */
 		for (numberOfGreenBottles = MAX_NUMBER_OF_GREEN_BOTTLES; numberOfGreenBottles > 0; numberOfGreenBottles--) {
 			System.out.printf("\n%d green bottles hanging on the wall,\n",
@@ -53,8 +52,8 @@ class TestClass {
 			System.out
 					.printf("And if one green bottle should accidentally fall,\n");
 			System.out.printf(
-					"There'll be %d green bottles hanging on the wall.\n", 
-					(numberOfGreenBottles-1));
+					"There'll be %d green bottles hanging on the wall.\n",
+					(numberOfGreenBottles - 1));
 		}
 	}
 }
