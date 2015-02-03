7dc5f3f2a6dc2d3749a63a76991e338556df0cc4
dk666 <dk666@york.ac.uk>
2015-02-03 14:26:59 +0000

    Re-arranged server class
    Signed-off-by: dk666 <dk666@york.ac.uk>

--- Presentation/src/server/server.java
+++ Presentation/src/server/server.java
@@ -9,14 +9,20 @@ package server;
  */
 public class server {
 	public static Console console;
+	private Slideshow currentSlideshow;
+	
+	public server() {
+		console = new Console();
+		XMLReader reader = new XMLReader();
+		currentSlideshow = reader.getSlideshow();
+	}
+	
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		console = new Console();
-		XMLReader reader = new XMLReader();
-
+		new server();
 	}
 
 }
