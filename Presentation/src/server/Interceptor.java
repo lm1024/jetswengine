7e85ed0aae0e6bdc7c90cff15c2dd8d7a462b8fe
dk666 <dk666@york.ac.uk>
2015-02-03 11:28:59 +0000

    Wrote interceptor to override standard output stream
    Signed-off-by: dk666 <dk666@york.ac.uk>

---
+++ Presentation/src/server/Interceptor.java
@@ -0,0 +1,18 @@
+package server;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+public class Interceptor extends PrintStream
+{
+    public Interceptor(OutputStream out)
+    {
+        super(out, true);
+    }
+    @Override
+    public void print(String s)
+    {
+        super.print(s);
+    	server.console.printToConsole("SYSTEM", s);
+    }
+}
