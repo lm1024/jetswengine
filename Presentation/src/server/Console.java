192418fbd841b703542f3001357d7fd38ee64076
dk666 <dk666@york.ac.uk>
2015-02-03 11:12:36 +0000

    Created public print to console function
    Signed-off-by: dk666 <dk666@york.ac.uk>

--- Presentation/src/server/Console.java
+++ Presentation/src/server/Console.java
@@ -1,15 +1,12 @@
 package server;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -18,7 +15,6 @@ import javax.swing.JTextField;
 public class Console extends JPanel implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel panel;
 	private JTextField textInput;
 	private JTextArea textOutput;
 	private JScrollPane areaScrollPane;
@@ -30,15 +26,24 @@ public class Console extends JPanel implements ActionListener {
 		
 		// Instantiate a JTextArea to output the data
 		textOutput = new JTextArea();
-		textOutput.setEditable(false); // Prevent the user from editing it
+		
+		// Prevent the user from editing it
+		textOutput.setEditable(false); 
+		
+		// Make text wrap by words
 		textOutput.setLineWrap(true);
 		textOutput.setWrapStyleWord(true);
+		
+		//make it visible
 		textOutput.setVisible(true);
 
+		
 		areaScrollPane = new JScrollPane(textOutput);
 		areaScrollPane
 				.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-		add(areaScrollPane, BorderLayout.CENTER); // Add the areaScrollPane to the JPanel
+		
+		// Add the areaScrollPane to the JPanel
+		add(areaScrollPane, BorderLayout.CENTER); 
 
 		// Instantiate a JTextFields for data entry
 		textInput = new JTextField(20);
@@ -49,9 +54,10 @@ public class Console extends JPanel implements ActionListener {
 		makeFrame();
 	}
 
-	// When text is entered or the button is pressed the following method will
-	// be called
-
+	/* 
+	 * When text is entered or the button is pressed
+	 * the following method will be called
+	 */
 	public void actionPerformed(ActionEvent e) {
 
 		// If the "Read value" button is pressed
@@ -65,9 +71,12 @@ public class Console extends JPanel implements ActionListener {
 
 		}
 	}
-
+	
+	public void printToConsole(String user, String message){
+		textOutput.append(user + ": " + message + "\n");
+	}
+	
 	// Create a window frame
-
 	public void makeFrame() {
 
 		// Instantiate a window frame using Swing class JFrame
