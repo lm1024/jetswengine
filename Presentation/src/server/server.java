b9da7112fe392fd114b834d8fcd9122916cd1d29
dk666 <dk666@york.ac.uk>
2015-02-03 09:35:23 +0000

    Create server class
    Signed-off-by: dk666 <dk666@york.ac.uk>

---
+++ Presentation/src/server/server.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package server;
+
+/**
+ * @author dk666
+ *
+ */
+public class server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
