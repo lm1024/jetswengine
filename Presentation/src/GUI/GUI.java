0471cac617c7a9ec1b6bb208c5f2b7bb3330cd28
jf878 <jf878@york.ac.uk>
2015-02-19 17:07:56 +0000

    Made use of grid layout. Put a second button in with both buttons have different outputs.

--- Presentation/src/GUI/GUI.java
+++ Presentation/src/GUI/GUI.java
@@ -3,13 +3,18 @@
  */
 package GUI;
 
+import java.awt.Insets;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Group;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
 
 /**
  * @author
@@ -42,18 +47,35 @@ public class GUI extends Application {
 		/* Set the title of the window */
 		primaryStage.setTitle("JavaFX Welcome");
 
-		Group group = new Group();
-		//group.setStyle("-fx-background-color: white;");
+		//Group group = new Group();
+		//group.setStyle("-fx-background-color: red;");
+		
+		//create a grid pane
+		GridPane grid = new GridPane();
 
 		/* Create new button */
 		Button btn = makeButton(100, 100, "Button Text");
 		btn.setId("button1"); // id set so that source of event can be found
 		btn.getText();
-		group.getChildren().add(btn);
+		grid.add(btn, 0, 0);
+		
+		/* Create new button */
+		Button btn1 = makeButton(200, 200, "Another Button");
+		btn1.setId("button2"); // id set so that source of event can be found
+		btn1.getText();
+		grid.add(btn1, 0, 1);
+		
 
-		Scene scene = new Scene(group, 500, 500);
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		//grid.setPadding(new Insets(25, 25, 25, 25));
 
-		primaryStage.setScene(scene);
+
+		//creates a scene within the stage of pixel size x by y
+		Scene mainScene = new Scene(grid, 400, 500);
+
+		primaryStage.setScene(mainScene);
 		
 		/* Line sets the screen to fullscreen */
 		//primaryStage.setFullScreen(true);
@@ -68,7 +90,7 @@ public class GUI extends Application {
 		/* Button section */
 		Button btn = new Button();
 		btn.setText(buttonText);
-		btn.relocate(400, 400);
+		btn.relocate(xPos, yPos);
 		btn.setOnAction(new buttonEventHandler());
 		return btn;
 	}
@@ -81,7 +103,15 @@ public class GUI extends Application {
 		@Override
 		public void handle(ActionEvent e) {
 			Button buttonPressed = (Button) e.getSource();
-			System.out.println(buttonPressed.getId());
+			
+			switch(buttonPressed.getId()) {
+				case "button1" : System.out.println("button1 pressed");
+					break;
+				case "button2" : System.out.println("button2 pressed");
+					break;
+					default: System.out.println("error");
+			}
+			
 		}
 	}
 
