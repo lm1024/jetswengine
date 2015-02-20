29360eb80403a4f513fa2e7c1c1dcb9d6a5d1657
Tom Davidson <tomdavidson@live.co.uk>
2015-02-20 21:48:17 +0000

    Finished the basic functionality of the html viewer. Still have to do the building of the handler, but I know how the html viewer works now.
    Signed-off-by: Tom Davidson <tomdavidson@live.co.uk>

--- Presentation/src/GUI/BindingsConcatTest.java
+++ Presentation/src/GUI/BindingsConcatTest.java
@@ -1,17 +1,18 @@
 package GUI;
 
-import java.awt.font.GlyphMetrics;
 import java.io.File;
+import java.lang.reflect.Field;
 import java.net.MalformedURLException;
 
+import com.sun.webpane.platform.WebPage;
+
+import netscape.javascript.JSException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.application.Platform;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.text.Font;
-import javafx.scene.web.HTMLEditor;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
@@ -19,64 +20,101 @@ public class BindingsConcatTest extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		/*
-		 * FlowPane flow = new FlowPane(); flow.setVgap(1); flow.setHgap(4);
-		 * flow.setPrefWrapLength(100); // preferred width = 300
-		 * flow.getChildren().addAll(label, label2);
-		 */
+		WebView webView = new WebView();
+		webView.setPrefWidth(200);
 
-		final HTMLEditor htmlEditor = new HTMLEditor();
-
-		htmlEditor.setPrefSize(400, 300);
-		htmlEditor.relocate(100, 100);
-
-		htmlEditor.setHtmlText(" <p>This text contains <sup>superscript</sup> text.</p> ");
-
-		final WebView webView = new WebView();
-		webView.setPrefSize(200, 200);
-		
 		File f = new File("custom.css");
 		try {
-		    webView.getEngine().setUserStyleSheetLocation(f.toURI().toURL().toString());
+			webView.getEngine().setUserStyleSheetLocation(f.toURI().toURL().toString());
 		} catch (MalformedURLException ex) {
-		    // not important here
+			// not important here
+			System.out.println("custom.css does not exist.");
 		}
-		
+
 		webView.setDisable(true);
 		webView.getEngine()
 				.loadContent(
-						"<body overflow-x: hidden; bgcolor=rgba(0, 0, 204, 0); contenteditable=\"false\"><p style=\"text-align: left;\"><sub>subscript</sub><sup>superscript</sup><font face=\"arial\" color=\"ff0000\"> color arial </font><span style=\"opacity:0.1\">opacity </span><span style=\"font-size:20px\">size 20 </span><span style=\"font-size:30px\">size 30 </span><p style=\"text-align: right;\">p tag alligned right </p><strike>strikethrough </strike> hjakfsdlhjlksadf hjklfdsa hjklsfd hjlkfds hjklfds hjklfdsah jlksdfah jlksafdh jlkfdsa hjlksfda hjkldfsah jlkfdsah  <a href=\"http://www.facebook.com\">link text</a> </p></body>");
-		
-		Font thisFont = new Font("arial", 20);
-		
-		
-		// <html> </html> -- not required
-		// <body bgcolor=\"#efe7b5\"; contenteditable=\"false\">STUFF GOES HERE </body>
-		// <span style=\"opacity:0.1\">TEXT GOES HERE</span>
-		// <font color=\"ff0000\">contains</font>
-		// <p style=\"text-align: left;\"> STUFF GOES HERE </p>
-		
+						"<div id=\"mydiv\"><body background-color: transparent; contenteditable=\"false\"><p style=\"text-align: left;\"><sub>subscript</sub><sup>superscript</sup><font face=\"arial\" color=\"ff0000\"> color arial </font><span style=\"opacity:0.1\">opacity </span><span style=\"font-size:20px\">size 20 </span><span style=\"font-size:30px\">size 30 </span><p style=\"text-align: right;\">p tag alligned right </p><strike>strikethrough </strike> hjakfsdlhjlksadf hjklfdsa hjklsfd hjlkfds hjklfds hjklfdsah jlksdfah jlksafdh jlkfdsa hjlksfda hjkldfsah jlkfdsah  <a href=\"http://www.facebook.com\">link text</a> </p></body></div>");
+
+		/*
+		 * <html> </html> -- not required <div id=\"mydiv\"> </div> -- Used for
+		 * calculating height of text... ESSENTIAL <body bgcolor=\"#efe7b5\";
+		 * contenteditable=\"false\">STUFFGOESHERE</body> <span
+		 * style=\"opacity:0.1\">TEXT GOES HERE</span> <font
+		 * color=\"ff0000\">contains</font> <p style=\"text-align:left;\"> STUFF
+		 * GOES HERE </p>
+		 */
+
 		webView.relocate(100, 100);
 		webView.setContextMenuEnabled(false);
+		adjustHeight(webView);
 
-		Button btn = new Button();
-		btn.setText("Submit");
-		btn.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				webView.getEngine().loadContent(htmlEditor.getHtmlText());
-				System.out.println(htmlEditor.getHtmlText());
-			}
-		});
+		/*
+		 * Remove the background from the webView panel. Adapted from
+		 * http://stackoverflow
+		 * .com/questions/12421250/transparent-background-in-
+		 * the-webview-in-javafx
+		 */
+		Field field;
+		try {
+			field = webView.getEngine().getClass().getDeclaredField("page");
+			field.setAccessible(true);
+			WebPage page = (WebPage) field.get(webView.getEngine());
+			page.setBackgroundColor((new java.awt.Color(0, 0, 0, 0)).getRGB());
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-		btn.relocate(500, 50);
+		/* This works, perhaps just say that backgrounds can be made using graphics handler? */
+		Rectangle rect = new Rectangle(100, 100, webView.getPrefWidth(), 200);
+		rect.setFill(new Color(1, 0, 1, 1));
 
 		Group root = new Group();
-		root.getChildren().addAll(webView);//, htmlEditor, btn);
+		root.getChildren().addAll(rect, webView);
 
 		Scene scene = new Scene(root, 500, 500);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+
+	/**
+	 * Method to resize webView based upon size of the HTML within div tags with
+	 * the id mydiv. Adapted from code from
+	 * java-no-makanaikata.blogspot.co.uk/2012/10/javafx-webview-size-trick.html
+	 */
+	private void adjustHeight(final WebView webView) {
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					/*
+					 * Runs Javascript command on the webView to get height of
+					 * the text enclosed by the mydiv element.
+					 */
+					Object result = webView.getEngine().executeScript("document.getElementById('mydiv').offsetHeight");
+					if (result instanceof Integer) {
+						Integer i = (Integer) result;
+						double height = new Double(i);
+						height = height + 20;
+						webView.setPrefHeight(height);
+					}
+				} catch (JSException e) {
+					System.out
+							.println("Exception occured whilst trying to resize a text box. Possibly caused by badly formed HTML div tag.");
+					System.exit(-1);
+				}
+			}
+		});
 	}
 
 	public static void main(String[] args) {
--- Presentation/src/GUI/GUI.java
+++ Presentation/src/GUI/GUI.java
@@ -1,88 +1,98 @@
-/**
- * 
- */
 package GUI;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Group;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Control;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.PaneBuilder;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-/**
- * @author
- * @version 1.0 19/02/2015
- * 
- */
 public class GUI extends Application {
+	
 
-	/**
-	 * 
-	 */
-	public GUI() {
-	}
-
-	/**
-	 * @param args
-	 */
+	private static final String SMALL_CONTENT = "Lorem ipsum dolor sit amet";
+	private static final String BIG_CONTENT = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod" +
+	" tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, " +
+	"quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo" +
+	" consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie" +
+	" consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto" +
+	" odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait" +
+	" nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet" +
+	" doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam;" +
+	" est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt " +
+	"lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus," +
+	" qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica," +
+	" quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula " +
+	"quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, " +
+	"fiant sollemnes in futurum.";
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
 
-	/**
-	 * Must must implement the inherited abstract method
-	 * Application.start(Stage).
-	 */
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		/* TODO Jake, please make a nice GUI. You are our only hope. */
+	public void start(Stage stage) throws Exception
+    {
+		final Text browser = new Text(SMALL_CONTENT);
 		
-		/* Set the title of the window */
-		primaryStage.setTitle("JavaFX Welcome");
-
-		Group group = new Group();
-		//group.setStyle("-fx-background-color: white;");
-
-		/* Create new button */
-		Button btn = makeButton(100, 100, "Button Text");
-		btn.setId("button1"); // id set so that source of event can be found
-		btn.getText();
-		group.getChildren().add(btn);
-
-		Scene scene = new Scene(group, 500, 500);
-
-		primaryStage.setScene(scene);
+		final Pane pane = PaneBuilder.create().children(browser).style("-fx-border-color: blue").build();  
 		
-		/* Line sets the screen to fullscreen */
-		//primaryStage.setFullScreen(true);
-
-		//thisGraphicsHandler.drawRectangle();
+		pane.widthProperty().addListener( new ChangeListener<Object>() {
+	    	public void changed(ObservableValue<?> observable, Object oldValue, Object newValue) 
+	    	{ 
+	    		browser.setPrefWidth((Double)newValue);
+	    	}    
+	    });
 		
-		primaryStage.show();
-	}
+		Button button = new Button("More..");
+	    button.setMinWidth(Control.USE_PREF_SIZE);
+	    button.setOnAction(new ButtonEventHandler(browser));
+		
+		
+		BorderPane border = new BorderPane();
+		border.setTop(button);
+		
+		VBox vbox = new VBox();
+		vbox.getChildren().add(pane);
+		vbox.setAlignment( Pos.TOP_CENTER );
+		border.setCenter(vbox);
+		
+		
+		Scene scene = new Scene(border, 800, 600);
+        
+        stage.setScene(scene);
+        stage.setTitle("Test");
+        stage.show();
+        
+    }
 
-	/** Utility function for adding button */
-	private Button makeButton(double xPos, double yPos, String buttonText) {
-		/* Button section */
-		Button btn = new Button();
-		btn.setText(buttonText);
-		btn.relocate(400, 400);
-		btn.setOnAction(new buttonEventHandler());
-		return btn;
-	}
+	
+	private class ButtonEventHandler implements EventHandler<ActionEvent> {
+		
+		private boolean toggle = false;
+		private Text browser;
 
-	/**
-	 * Private class written so that multiple buttonEvents can be handled
-	 * easily.
-	 */
-	private class buttonEventHandler implements EventHandler<ActionEvent> {
+		public ButtonEventHandler(Text browser )
+		{
+			this.browser = browser;
+		}
+		
 		@Override
 		public void handle(ActionEvent e) {
-			Button buttonPressed = (Button) e.getSource();
-			System.out.println(buttonPressed.getId());
+			toggle = !toggle;
+			if (toggle) {
+				browser.setContent(	BIG_CONTENT);
+				
+			} else {
+				browser.setContent(SMALL_CONTENT);
+			}
 		}
 	}
-
 }
