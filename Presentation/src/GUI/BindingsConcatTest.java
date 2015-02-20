ba77a11e31de197b419a04306aed6c431d9a5eee
solomonatron <tomdavidson@live.co.uk>
2015-02-20 15:29:54 +0000

    Worked on text handler, investigated html tags / webview as way of displaying text.
    Signed-off-by: solomonatron <tomdavidson@live.co.uk>

--- Presentation/src/GUI/BindingsConcatTest.java
+++ Presentation/src/GUI/BindingsConcatTest.java
@@ -1,20 +1,16 @@
 package GUI;
 
-import java.util.regex.Pattern;
+import java.awt.font.GlyphMetrics;
+import java.io.File;
+import java.net.MalformedURLException;
 
 import javafx.application.Application;
-import javafx.beans.binding.Bindings;
-import javafx.geometry.Orientation;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.text.Font;
 import javafx.scene.web.HTMLEditor;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
@@ -23,87 +19,65 @@ public class BindingsConcatTest extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		TextField tf1 = new TextField();
-		Label label = new Label();
-
-		label.textProperty().bind(tf1.textProperty());
-		// label.setMaxHeight(50);
-		label.setMaxWidth(100);
-		label.setWrapText(true);
-		label.relocate(100, 100);
-
-		// Label label2 = new Label("Test label whoo lots of words");
-		// label2.setWrapText(false);
-		// label2.relocate(100, 100);
-
 		/*
 		 * FlowPane flow = new FlowPane(); flow.setVgap(1); flow.setHgap(4);
 		 * flow.setPrefWrapLength(100); // preferred width = 300
 		 * flow.getChildren().addAll(label, label2);
 		 */
 
-		HTMLEditor htmlEditor = new HTMLEditor();
-		htmlEditor.setPrefSize(200, 100);
+		final HTMLEditor htmlEditor = new HTMLEditor();
+
+		htmlEditor.setPrefSize(400, 300);
 		htmlEditor.relocate(100, 100);
-		htmlEditor.setStyle("-fx-border: transparent;" + "-top-toolbar: transparant");
-		
-		highlight(htmlEditor);
 
 		htmlEditor.setHtmlText(" <p>This text contains <sup>superscript</sup> text.</p> ");
 
-		// flow.relocate(0, 50);
-
-		WebView webView = new WebView();
+		final WebView webView = new WebView();
 		webView.setPrefSize(200, 200);
-		webView.getEngine().loadContent("<html><body bgcolor=#000000></body></html>");
-		webView.relocate(100, 220);
 		
-		Group root = new Group();
-		root.getChildren().addAll(tf1, htmlEditor, webView);
+		File f = new File("custom.css");
+		try {
+		    webView.getEngine().setUserStyleSheetLocation(f.toURI().toURL().toString());
+		} catch (MalformedURLException ex) {
+		    // not important here
+		}
+		
+		webView.setDisable(true);
+		webView.getEngine()
+				.loadContent(
+						"<body overflow-x: hidden; bgcolor=rgba(0, 0, 204, 0); contenteditable=\"false\"><p style=\"text-align: left;\"><sub>subscript</sub><sup>superscript</sup><font face=\"arial\" color=\"ff0000\"> color arial </font><span style=\"opacity:0.1\">opacity </span><span style=\"font-size:20px\">size 20 </span><span style=\"font-size:30px\">size 30 </span><p style=\"text-align: right;\">p tag alligned right </p><strike>strikethrough </strike> hjakfsdlhjlksadf hjklfdsa hjklsfd hjlkfds hjklfds hjklfdsah jlksdfah jlksafdh jlkfdsa hjlksfda hjkldfsah jlkfdsah  <a href=\"http://www.facebook.com\">link text</a> </p></body>");
+		
+		Font thisFont = new Font("arial", 20);
 		
 		
+		// <html> </html> -- not required
+		// <body bgcolor=\"#efe7b5\"; contenteditable=\"false\">STUFF GOES HERE </body>
+		// <span style=\"opacity:0.1\">TEXT GOES HERE</span>
+		// <font color=\"ff0000\">contains</font>
+		// <p style=\"text-align: left;\"> STUFF GOES HERE </p>
+		
+		webView.relocate(100, 100);
+		webView.setContextMenuEnabled(false);
 
-		Scene scene = new Scene(root, 250, 250);
+		Button btn = new Button();
+		btn.setText("Submit");
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				webView.getEngine().loadContent(htmlEditor.getHtmlText());
+				System.out.println(htmlEditor.getHtmlText());
+			}
+		});
+
+		btn.relocate(500, 50);
+
+		Group root = new Group();
+		root.getChildren().addAll(webView);//, htmlEditor, btn);
+
+		Scene scene = new Scene(root, 500, 500);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		Node a = htmlEditor.lookup(".tool-bar");
-		dump(a);
-		/* Black magic */
-		for (Node toolBar = htmlEditor.lookup(".tool-bar"); toolBar != null; toolBar = htmlEditor.lookup(".tool-bar")) {
-			System.out.println(toolBar);
-			((Pane) toolBar.getParent()).getChildren().remove(toolBar);
-		}
 	}
-	
-	/** Debugging routine to dump the scene graph. */
-    public static void dump(Node n) {
-        dump(n, 0);
-    }
-
-    private static void dump(Node n, int depth) {
-        for (int i = 0; i < depth; i++) System.out.print("  ");
-        System.out.println(n);
-        if (n instanceof Parent) {
-            for (Node c : ((Parent) n).getChildrenUnmodifiable()) {
-                dump(c, depth + 1);
-            }
-        }
-    }
-
-    /** Debugging routine to highlight the borders of nodes. **/
-    public static void highlight(Node n) {
-        highlight(n, 0);
-    }
-
-    private static void highlight(Node n, int depth) {
-        n.setStyle("-fx-stroke: red; -fx-stroke-width: 1; -fx-stroke-type: inside;");
-        if (n instanceof Parent) {
-            for (Node c : ((Parent) n).getChildrenUnmodifiable()) {
-                highlight(c, depth + 1);
-            }
-        }
-    }
-
 
 	public static void main(String[] args) {
 		launch(args);
