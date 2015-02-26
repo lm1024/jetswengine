729c4c800194d504bc8fc08bb0cfceccffb5dc13
ipf501 <ipf501@york.ac.uk>
2015-02-26 18:09:03 +0000

    Created getimage function
    Signed-off-by: ipf501 <ipf501@york.ac.uk>

--- Presentation/src/GUI/GUI.java
+++ Presentation/src/GUI/GUI.java
@@ -74,9 +74,10 @@ public class GUI extends Application {
 
 		
 		//Company icon in column 1-3, row 1-3
-		Image companylogo = new Image("file:WM_logo_transparent.png", 225, 225, true, true);
-		ImageView companyimage = new ImageView();
-		companyimage.setImage(companylogo);
+		//Image companylogo = new Image("file:WM_logo_transparent.png", 225, 225, true, true);
+		//ImageView companyimage = new ImageView();
+		//companyimage.setImage(companylogo);
+		ImageView companyimage = getimage("file:WM_logo_transparent.png", 225, 225);
 		grid.add(companyimage, 0, 0, 3, 3);
 		
 		
@@ -85,9 +86,10 @@ public class GUI extends Application {
 		grid.add(productName, 1, 2, 4, 1);
 	
 		//Create first button for Slide Preview and add in column 1, row 4
-		Image buttonimage1 = new Image("file:me.png", 130, 150, true, true);
-		ImageView presentationimage1 = new ImageView();
-		presentationimage1.setImage(buttonimage1);
+		//Image buttonimage1 = new Image("file:me.png", 130, 150, true, true);
+		//ImageView presentationimage1 = new ImageView();
+		//presentationimage1.setImage(buttonimage1);
+		ImageView presentationimage1 = getimage("file:me.png", 130, 150);
 		Button one = makeButton("Presentation 1", "invisiButton", true);
 		one.setStyle("-fx-border-width: 0;");
 		one.setGraphic(presentationimage1);
@@ -102,9 +104,10 @@ public class GUI extends Application {
 		grid.add(emptycell, 1, 3, 1, 3);
 		
 		//Create second button for Slide Preview and add in column 3, row 4
-		Image buttonimage2 = new Image("file:me.png", 130, 150, true, true);
-		ImageView presentationimage2 = new ImageView();
-		presentationimage2.setImage(buttonimage2);
+		//Image buttonimage2 = new Image("file:me.png", 130, 150, true, true);
+		//ImageView presentationimage2 = new ImageView();
+		//presentationimage2.setImage(buttonimage2);
+		ImageView presentationimage2 = getimage("file:me.png", 130, 150);
 		Button two = makeButton("Presentation 2", "invisiButton", true);
 		two.setStyle("-fx-border-width: 0;");
 		two.setGraphic(presentationimage2);
@@ -119,9 +122,10 @@ public class GUI extends Application {
 		grid.add(emptycell1, 3, 3, 1, 3);
 				
 		//Create third button for Slide Preview and add in column 5, row 4
-		Image buttonimage3 = new Image("file:me.png", 130, 150, true, true);
-		ImageView presentationimage3 = new ImageView();
-		presentationimage3.setImage(buttonimage3);
+		//Image buttonimage3 = new Image("file:me.png", 130, 150, true, true);
+		//ImageView presentationimage3 = new ImageView();
+		//presentationimage3.setImage(buttonimage3);
+		ImageView presentationimage3 = getimage("file:me.png", 130, 150);
 		Button three = makeButton("Presentation 3", "invisiButton", true);
 		three.setGraphic(presentationimage3);
 		three.setStyle("-fx-border-width: 0;");
@@ -136,9 +140,10 @@ public class GUI extends Application {
 		grid.add(emptyrow, 0, 4, 5, 1);
 		
 		//Create forth button for Slide Preview and add in column 1, row 6
-		Image buttonimage4 = new Image("file:me.png", 130, 150, true, true);
-		ImageView presentationimage4 = new ImageView();
-		presentationimage4.setImage(buttonimage4);
+		//Image buttonimage4 = new Image("file:me.png", 130, 150, true, true);
+		//ImageView presentationimage4 = new ImageView();
+		//presentationimage4.setImage(buttonimage4);
+		ImageView presentationimage4 = getimage("file:me.png", 130, 150);
 		Button four = makeButton("Presentation 4", "invisiButton", true);
 		four.setGraphic(presentationimage4);
 		four.setStyle("-fx-border-width: 0;");
@@ -147,9 +152,10 @@ public class GUI extends Application {
 		grid.add(four, 0, 5, 1, 1);
 		
 		//Create fifth button for Slide Preview and add in column 3, row 6
-		Image buttonimage5 = new Image("file:me.png", 130, 150, true, true);
-		ImageView presentationimage5 = new ImageView();
-		presentationimage5.setImage(buttonimage5);
+		//Image buttonimage5 = new Image("file:me.png", 130, 150, true, true);
+		//ImageView presentationimage5 = new ImageView();
+		//presentationimage5.setImage(buttonimage5);
+		ImageView presentationimage5 = getimage("file:me.png", 130, 150);
 		Button five = makeButton("Presentation 5", "invisiButton", true);
 		five.setGraphic(presentationimage5);
 		five.setStyle("-fx-border-width: 0;");
@@ -158,9 +164,10 @@ public class GUI extends Application {
 		grid.add(five, 2, 5, 1, 1);
 		
 		//Create sixth button for Slide Preview and add in column 5, row 6
-		Image buttonimage6 = new Image("file:me.png", 130, 150, true, true);
-		ImageView presentationimage6 = new ImageView();
-		presentationimage6.setImage(buttonimage6);
+		//Image buttonimage6 = new Image("file:me.png", 130, 150, true, true);
+		//ImageView presentationimage6 = new ImageView();
+		//presentationimage6.setImage(buttonimage6);
+		ImageView presentationimage6 = getimage("file:me.png", 130, 150);
 		Button six = makeButton("Presentation 6", "invisiButton", true);
 		six.setGraphic(presentationimage6);
 		six.setStyle("-fx-border-width: 0;");
@@ -196,14 +203,13 @@ public class GUI extends Application {
 		return lbl;
 	}
 	
-	/** Utility function for adding button */
-	private Button makeButton(String buttonText, String styleClass) {
-		/* Button section */
-		Button btn = new Button();
-		btn.setText(buttonText);
-		btn.getStyleClass().add(styleClass);
-		btn.setOnAction(new buttonEventHandler());
-		return btn;
+	/** Utility function for getting images **/
+	private ImageView getimage(String imagefilename, double width, double height){
+		/* image section */
+		Image image = new Image(imagefilename, width, height, true, true);
+		ImageView imageView = new ImageView();
+		imageView.setImage(image);
+		return imageView;
 	}
 
 	/**
@@ -235,6 +241,12 @@ public class GUI extends Application {
 		return btn;
 	}
 	
+	/**
+	 * 
+	 * Handler that applies and removes shading when mouse is hovering over a
+	 * button
+	 * 
+	 */
 	private class hoverHandler implements EventHandler<MouseEvent>{
 
 		@Override
