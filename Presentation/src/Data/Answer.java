ea834c8bc7c47e6cc0a483676e065003a3aa15a8
solomonatron <tomdavidson@live.co.uk>
2015-04-14 19:57:25 +0100

    Added question and answer to the data structure. Also wrote a small junit thing for testing the question class.
    Signed-off-by: solomonatron <tomdavidson@live.co.uk>

---
+++ Presentation/src/Data/Answer.java
@@ -0,0 +1,44 @@
+package Data;
+
+public class Answer {
+
+	private String id = null;
+	private Boolean correct;
+
+	private int answerCount;
+
+	public Answer(String id, boolean correct) {
+		/* Set the variables based upon the constructor arguments */
+		this.id = id;
+		this.correct = correct;
+	}
+	
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @return the correct
+	 */
+	public Boolean getCorrect() {
+		return correct;
+	}
+
+	/**
+	 * 
+	 */
+	public void increaseAnswerCount() {
+		answerCount++;
+	}
+
+	/**
+	 * @return answerCount
+	 */
+	public int getAnswerCount() {
+		return answerCount;
+	}
+
+}
