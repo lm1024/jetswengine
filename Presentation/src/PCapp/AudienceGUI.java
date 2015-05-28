6f29263badf1ce9fa3667d79432f5d468f5b0162
JakeFeasey <JakeFeasey@10.240.162.122>
2015-05-28 22:09:58 +0100

    Final audience GUI Code

--- Presentation/src/PCapp/AudienceGUI.java
+++ Presentation/src/PCapp/AudienceGUI.java
@@ -14,7 +14,6 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
