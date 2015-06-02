b358e4d17df8c270af945fdb7cba832dcc170cb6
harrisonhm <hazz3r@live.co.uk>
2015-06-02 22:04:35 +0100

    Update CustomOnItemSelectedListener.java
    Comments

--- StudentApp/app/src/main/java/com/wavemedia/studentapp/CustomOnItemSelectedListener.java
+++ StudentApp/app/src/main/java/com/wavemedia/studentapp/CustomOnItemSelectedListener.java
@@ -1,10 +1,4 @@
-/**
- *
- * Harrison Holt-McHale
- *
- * Copyright (c) 2015 WaveMedia. All rights reserved
- *
- */
+/** Copyright (c) 2015 WaveMedia. All rights reserved */
 
 package com.wavemedia.studentapp;
 
@@ -14,29 +8,35 @@ import android.view.View;
 import android.widget.AdapterView;
 
 /**
+ * Class for Listening to the Institution Dropdown Box
+ * 
  * @author Harrison Holt-McHale
  * @version 1.0 29/05/2015
  *
  */
-
-/* Class for Listening to the Institution Dropdown Box */
 public class CustomOnItemSelectedListener extends Activity implements AdapterView.OnItemSelectedListener {
 
     /* Instantiate selected value for dropdown box as position 0. */
     int selectedValue = 0;
 
-    /* Getter to get the selected value */
+    /**
+     * Getter to get the selected value 
+     */
     public int getSelectedValue(){
         return selectedValue;
     }
 
-    /* When the user selects an item in the dropdown box, updated selected Value */
+    /**
+     * When the user selects an item in the dropdown box, updated selected Value 
+     */
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         selectedValue = position;
     }
 
-    /* Required method to implement OnItemSelectedListener */
+    /* 
+     * Required method to implement OnItemSelectedListener 
+     */
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
     }
